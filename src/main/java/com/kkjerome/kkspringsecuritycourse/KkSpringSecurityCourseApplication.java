0589607df11bb49a2e66e589ddd321234dc48243
zachariah11jerobert <zachariah11jerobert@gmail.com>
2022-09-15 04:53:11 +0530

    basic credentials for spring security at application property file

--- src/main/java/com/kkjerome/kkspringsecuritycourse/KkSpringSecurityCourseApplication.java
+++ src/main/java/com/kkjerome/kkspringsecuritycourse/KkSpringSecurityCourseApplication.java
@@ -2,8 +2,10 @@ package com.kkjerome.kkspringsecuritycourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class KkSpringSecurityCourseApplication {
 
     public static void main(String[] args) {

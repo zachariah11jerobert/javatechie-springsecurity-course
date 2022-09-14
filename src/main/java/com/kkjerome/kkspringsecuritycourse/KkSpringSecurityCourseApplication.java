61ad7226a2291db544fe270be9166039fa15759e
unknown <startup.jerome@gmail.com>
2022-09-15 04:26:11 +0530

    initial commit of project

---
+++ src/main/java/com/kkjerome/kkspringsecuritycourse/KkSpringSecurityCourseApplication.java
@@ -0,0 +1,13 @@
+package com.kkjerome.kkspringsecuritycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkSpringSecurityCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkSpringSecurityCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkjerome/kkspringsecuritycourse/KkSpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkjerome.kkspringsecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkSpringSecurityCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
